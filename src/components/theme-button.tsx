2a5f397f5740236c038f754be271addec7942aef
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2025-10-10 17:42:46 +0200

    remove old shadcn, assets and others

--- src/components/theme-button.tsx
+++ src/components/theme-button.tsx
@@ -5,11 +5,7 @@ import { useTheme } from "next-themes"
 export function ThemeButton() {
   const { resolvedTheme, setTheme } = useTheme()
   return (
-    <button
-      type="button"
-      onClick={() => setTheme(resolvedTheme === "light" ? "dark" : "light")}
-      className="text-foreground hover:text-accent-foreground"
-    >
+    <button type="button" onClick={() => setTheme(resolvedTheme === "light" ? "dark" : "light")}>
       <SunIcon className="block h-6 w-6 dark:hidden" />
       <MoonIcon className="hidden h-6 w-6 dark:block" />
     </button>
