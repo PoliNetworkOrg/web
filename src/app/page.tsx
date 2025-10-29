d4bcc64f33cb6df1070d387cf5757ad1631fc997
Lorenzo Corallo <66379281+lorenzocorallo@users.noreply.github.com>
2025-10-29 12:26:25 +0100

    feat: add shadcn for components (#21)
    * initialize shadcn
    
    * feat: create css variables and tw utilities based on figma variables
    
    * docs: create GUIDES.md to explain how to build UI starting from Figma
    
    * fix: biome wants every CSS rule to end with a semicolon
    
    * docs: add important note in the create component part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 export default function Home() {
   return (
     <main className="container mx-auto px-4 py-8">
-      <h2 className="mb-4 text-3xl font-bold">Welcome to PoliNetwork</h2>
+      <h2 className="mb-4 typo-headline-large font-bold bg">Welcome to PoliNetwork</h2>
       <p className="mb-4">
         PoliNetwork is a student association dedicated to connecting and supporting students at Politecnico.
       </p>
+      <div className="bg-red rounded-rectangles w-90 py-8 flex justify-center items-center flex-col gap-2">
+        <p className="text-text-accent-darkbg typo-body-medium">Test with figma variables</p>
+        <div className="bg-background-blur w-20 h-20 rounded-images" />
+        <button
+          className="bg-button-primary text-text-accent-lightbg typo-label-large px-6 py-2.5 rounded-buttonsM"
+          type="button"
+        >
+          More groups
+        </button>
+      </div>
     </main>
   )
 }
