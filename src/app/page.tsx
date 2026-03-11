f34b6305739b3a12c45b0ba7abc6a65d095d8d6c
diubi <alessandroogioiaa@gmail.com>
2026-03-11 22:41:52 +0100

    Card initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,45 @@
-import { Hero } from "@/components/home/hero"
+import { CropIcon, Users } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { ButtonWithIcon } from "@/components/ui/buttonWithIcon"
+import {
+  Card,
+  CardAction,
+  CardBottomButton,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Home() {
-  return <Hero />
+  return (
+    <main className="container mx-auto px-4 py-8">
+      <h2 className="mb-4 typo-headline-large font-bold bg">Welcome to PoliNetwork</h2>
+      <p className="mb-4">
+        PoliNetwork is a student association dedicated to connecting and supporting students at Politecnico.
+      </p>
+      <div className="bg-red rounded-rectangles w-90 py-8 flex justify-center items-center flex-col gap-2">
+        <p className="text-text-accent-darkbg typo-body-medium">Test with figma variables</p>
+        <div className="bg-background-blur w-20 h-20 rounded-images" />
+        <ButtonWithIcon variant="primary" icon={Users} iconPosition="left" text="Diventa socio" />
+        <ButtonWithIcon variant="tertiary" icon={Users} iconPosition="right" text="Diventa socio" />
+        <ButtonWithIcon variant="tertiaryBlur" icon={Users} iconPosition="left" text="Diventa socio" />
+        <Button variant="link">Link</Button>
+      </div>
+
+      <Card className="w-64">
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardAction icon={CropIcon}></CardAction>
+        </CardHeader>
+        <CardContent>
+          <p>
+            description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidunt ut
+            labore et dolore magna aliqua.
+          </p>
+        </CardContent>
+        <CardBottomButton variant="tertiary">Button</CardBottomButton>
+      </Card>
+    </main>
+  )
 }
