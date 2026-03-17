9d80222ef9dbb9442d206e82c6da08ec47fcdd52
diubi <alessandroogioiaa@gmail.com>
2026-03-17 23:01:11 +0100

    Moved card.tsx to components/ui/cards

--- src/components/ui/cards/card.tsx
+++ src/components/ui/cards/card.tsx
@@ -1,8 +1,8 @@
 import type { LucideIcon } from "lucide-react"
 import * as React from "react"
 import { cn } from "@/lib/utils"
-import { Glass } from "../glass"
-import { Button } from "./button"
+import { Glass } from "../../glass"
+import { Button } from "../button"
 
 function Card({
   className,
