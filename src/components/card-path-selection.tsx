522ffc5f5d2ca282d536ebdf5e2e846ed2d5c8f0
diubi <alessandroogioiaa@gmail.com>
2026-04-01 00:19:44 +0200

    Added path selection cards

---
+++ src/components/card-path-selection.tsx
@@ -0,0 +1,14 @@
+import type { IconType } from "react-icons"
+import { Card, CardAction, CardContent } from "./ui/card"
+import { BsBook } from "react-icons/bs"
+
+export function CardPathSelection({ caption, icon: Icon = BsBook }: { caption: string; icon?: IconType }) {
+  return (
+    <Card className="bg-background-blur w-136 py-7.5 px-10 h-fit">
+      <CardContent className="flex flex-row justify-center items-center gap-1.25 text-[1rem] leading-6 tracking-[0.03rem] font-normal whitespace-nowrap">
+        <CardAction icon={Icon} iconSize="small" /> {caption}
+      </CardContent>
+    </Card>
+    //TODO: add hover effect and check if bg-color and icon color are aligned with figma
+  )
+}
