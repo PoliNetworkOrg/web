d4b9088e107a0ab449240fb6175a9eb44e55dc51
diubi <alessandroogioiaa@gmail.com>
2026-04-07 18:04:24 +0200

    fix: typography

--- src/components/card-path-selection.tsx
+++ src/components/card-path-selection.tsx
@@ -5,10 +5,10 @@ import { Card, CardAction, CardContent } from "./ui/card"
 export function CardPathSelection({ caption, icon: Icon = BsBook }: { caption: string; icon?: IconType }) {
   return (
     <Card className="h-fit w-136 bg-background-blur px-10 py-7.5">
-      <CardContent className="flex flex-row items-center justify-center gap-1.25 whitespace-nowrap font-normal text-[1rem] leading-6 tracking-[0.03rem]">
+      <CardContent className="typo-body-large flex flex-row items-center justify-center gap-1.25 whitespace-nowrap font-normal leading-6 tracking-[0.03125rem]">
         <CardAction icon={Icon} iconSize="small" /> {caption}
       </CardContent>
     </Card>
-    //TODO: add hover effect and check if bg-color and icon color are aligned with figma
+    //TODO: add hover effect
   )
 }
