0aa232543b6949510844001ee2796cb7c4603e25
diubi <alessandroogioiaa@gmail.com>
2026-04-09 17:10:13 +0200

    remove: bg-background-blur

--- src/components/card-path-selection.tsx
+++ src/components/card-path-selection.tsx
@@ -4,7 +4,7 @@ import { Card, CardAction, CardContent } from "./ui/card"
 
 export function CardPathSelection({ caption, icon: Icon = BsBook }: { caption: string; icon?: IconType }) {
   return (
-    <Card className="h-fit w-136 bg-background-blur px-10 py-7.5">
+    <Card className="h-fit w-136 px-10 py-7.5">
       <CardContent className="typo-body-large flex flex-row items-center justify-center gap-1.25 whitespace-nowrap font-normal leading-6 tracking-[0.03125rem]">
         <CardAction icon={Icon} iconSize="small" /> {caption}
       </CardContent>
