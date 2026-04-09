3f4cd674d8bd7e08598e95dffb485721eb636482
Bianca <ianoselbiancaroberta02@gmail.com>
2026-04-09 09:30:06 +0200

    Revert "fix: correct HTML entity in Not Found page"
    This reverts commit c194cbbeb6cfa1ba387ae3df054c489857f5ffd1.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default async function NotFound() {
     <main className="container mx-auto px-4 py-8">
       <h2 className="mb-4 font-bold text-3xl text-accent-foreground">Not Found</h2>
 
-      <p className="mb-4">This page doesn't exist or is not available yet.</p>
+      <p className="mb-4">This page doesn&apos;t exist or is not available yet.</p>
 
       <Link href="/">Go to homepage</Link>
     </main>
