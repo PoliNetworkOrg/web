32fe53b638e34478f2a776e27e27ddd4b4153deb
Bianca <ianoselbiancaroberta02@gmail.com>
2026-04-14 11:49:27 +0200

    fix:  biome

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,17 +1,9 @@
-
-import { AboutUs } from "@/components/home/about-us"
-import { Hero } from "@/components/home/hero"
-import { Materials } from "@/components/home/materials"
 import { CommunityNews } from "@/components/projects/community-news"
 
 export default function Home() {
-    return (
-        <main className="w-full">
-            <CommunityNews />
-            <Hero />
-            <Materials />
-            <AboutUs />
-
-        </main>
-    )
+  return (
+    <main className="w-full">
+      <CommunityNews />
+    </main>
+  )
 }
