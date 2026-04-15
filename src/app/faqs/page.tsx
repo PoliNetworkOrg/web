3b9edf2a63d13925efe2891e126ffcb49488be0f
Bianca <ianoselbiancaroberta02@gmail.com>
2026-04-15 11:53:11 +0200

    feat: add Item component structure based on shadcn

---
+++ src/app/faqs/page.tsx
@@ -0,0 +1,25 @@
+import { FiArrowUpRight } from "react-icons/fi"
+import { Button } from "@/components/ui/button"
+import { Item, ItemActions, ItemContent, ItemInner, ItemTitle } from "@/components/ui/item"
+
+export default function FAQsPage() {
+  return (
+    <main className="w-full">
+      <div className="flex flex-col items-center py-12">
+        <Item className="flex w-full max-w-4xl flex-col">
+          <ItemInner>
+            <ItemContent>
+              <ItemTitle>Non trovi ciò che stai cercando?</ItemTitle>
+            </ItemContent>
+            <ItemActions>
+              <Button variant="primary" size="lg" className="w-fit">
+                Esplora le Guide
+                <FiArrowUpRight />
+              </Button>
+            </ItemActions>
+          </ItemInner>
+        </Item>
+      </div>
+    </main>
+  )
+}
