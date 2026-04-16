2211cd8aea1c8da57ac54c46bd590f77bb530c06
Bianca <ianoselbiancaroberta02@gmail.com>
2026-04-16 14:43:02 +0200

    feat: add multiple associations with content and links to the AssociationsPage

--- src/app/associations/page.tsx
+++ src/app/associations/page.tsx
@@ -83,14 +83,87 @@ const accordionItems = [
         icon: FiGlobe,
       },
     ],
+  }, {
+    value: "Lista Aperta - Per il diritto allo studio",
+    name: "Lista Aperta - Per il diritto allo studio",
+    logo: esnLogo,
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Velit integer diam in id proin blandit fames id. Volutpat at vel risus non amet tortor. Potenti sit gravida donec lacinia et posuere faucibus. Elementum libero diam nullam ultricies mauris mauris erat porttitor. At morbi commodo nunc vulputate id odio pellentesque ipsum. Adipiscing at dictumst pulvinar mattis faucibus quisque donec convallis commodo. ",
+    links: [
+      {
+        key: "Facebook",
+        href: "https://www.google.com",
+        icon: FiFacebook,
+      },
+    ],
+  }, {
+    value: "MESA - EESTEC",
+    name: "MESA - EESTEC",
+    logo: esnLogo,
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Velit integer diam in id proin blandit fames id. Volutpat at vel risus non amet tortor. Potenti sit gravida donec lacinia et posuere faucibus. Elementum libero diam nullam ultricies mauris mauris erat porttitor. At morbi commodo nunc vulputate id odio pellentesque ipsum. Adipiscing at dictumst pulvinar mattis faucibus quisque donec convallis commodo. ",
+    links: [
+      {
+        key: "Facebook",
+        href: "https://www.google.com",
+        icon: FiFacebook,
+      },
+    ],
+  }, {
+    value: "Polifonia",
+    name: "Polifonia",
+    logo: esnLogo,
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Velit integer diam in id proin blandit fames id. Volutpat at vel risus non amet tortor. Potenti sit gravida donec lacinia et posuere faucibus. Elementum libero diam nullam ultricies mauris mauris erat porttitor. At morbi commodo nunc vulputate id odio pellentesque ipsum. Adipiscing at dictumst pulvinar mattis faucibus quisque donec convallis commodo. ",
+    links: [
+      {
+        key: "Facebook",
+        href: "https://www.google.com",
+        icon: FiFacebook,
+      },
+    ],
+  }, {
+    value: "POLI.RADIO",
+    name: "POLI.RADIO",
+    logo: esnLogo,
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Velit integer diam in id proin blandit fames id. Volutpat at vel risus non amet tortor. Potenti sit gravida donec lacinia et posuere faucibus. Elementum libero diam nullam ultricies mauris mauris erat porttitor. At morbi commodo nunc vulputate id odio pellentesque ipsum. Adipiscing at dictumst pulvinar mattis faucibus quisque donec convallis commodo. ",
+    links: [
+      {
+        key: "Facebook",
+        href: "https://www.google.com",
+        icon: FiFacebook,
+      },
+    ],
+  }, {
+    value: "Studenti Indipendenti",
+    name: "Studenti Indipendenti",
+    logo: esnLogo,
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Velit integer diam in id proin blandit fames id. Volutpat at vel risus non amet tortor. Potenti sit gravida donec lacinia et posuere faucibus. Elementum libero diam nullam ultricies mauris mauris erat porttitor. At morbi commodo nunc vulputate id odio pellentesque ipsum. Adipiscing at dictumst pulvinar mattis faucibus quisque donec convallis commodo. ",
+    links: [
+      {
+        key: "Facebook",
+        href: "https://www.google.com",
+        icon: FiFacebook,
+      },
+    ],
   },
 ]
 
 export default function AssociationsPage() {
   return (
     <main className="w-full">
-      <div className="mx-auto w-300 py-12">
-        <AccordionAssociation accordionItems={accordionItems} defaultValue="ESN - Erasmus Student Network" />
+      <div className="mx-auto flex min-h-screen max-w-400 flex-col items-center justify-center gap-39 px-4 py-49">
+        <div className="flex flex-col items-center gap-6">
+          <h2 className="typo-display-extralarge w-fit bg-linear-to-b from-text-primary to-text-secondary bg-clip-text py-14 text-transparent">
+            Associazioni
+          </h2>
+          <p className="typo-headline-small max-w-xl text-center">Scopri le associazioni studentesche del Politecnico</p>
+        </div>
+        <div className="w-full max-w-300">
+          <AccordionAssociation accordionItems={accordionItems} defaultValue="ESN - Erasmus Student Network" />
+        </div>
       </div>
     </main>
   )
