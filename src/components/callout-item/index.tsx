f61a2caa9d29b53056ccf402ba580836755ee7da
Bianca <ianoselbiancaroberta02@gmail.com>
2026-04-16 14:11:27 +0200

    fix: replace useRouter with Link for navigation in CalloutItem component

--- src/components/callout-item/index.tsx
+++ src/components/callout-item/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useRouter } from "next/navigation"
+import Link from "next/link"
 import { FiArrowUpRight } from "react-icons/fi"
 import { Button } from "@/components/ui/button"
 import { Item, ItemActions, ItemContent, ItemInner, ItemTitle } from "@/components/ui/item"
@@ -8,8 +8,6 @@ import { cn } from "@/lib/utils"
 import type { CalloutItemProps } from "./types"
 
 export default function CalloutItem({ title, href, buttonText, className }: CalloutItemProps) {
-  const router = useRouter()
-
   return (
     <Item className={cn("flex w-full flex-col", className)}>
       <ItemInner>
@@ -17,14 +15,11 @@ export default function CalloutItem({ title, href, buttonText, className }: Call
           <ItemTitle>{title}</ItemTitle>
         </ItemContent>
         <ItemActions>
-          <Button
-            variant="primary"
-            size="lg"
-            className="gap-8 pr-6 pl-10 has-[>svg]:pr-6 has-[>svg]:pl-10"
-            onClick={() => router.push(href)}
-          >
-            {buttonText}
-            <FiArrowUpRight aria-hidden="true" />
+          <Button variant="primary" size="lg" asChild className="gap-8 pr-6 pl-10 has-[>svg]:pr-6 has-[>svg]:pl-10">
+            <Link href={href}>
+              {buttonText}
+              <FiArrowUpRight aria-hidden="true" />
+            </Link>
           </Button>
         </ItemActions>
       </ItemInner>
