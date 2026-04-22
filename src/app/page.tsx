5b90c64910f72d83634e5b5b2f60ec485e33eb97
diubi <alessandroogioiaa@gmail.com>
2026-04-22 22:25:16 +0200

    remove: test CTAs in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
 import Image from "next/image"
-import { FaRegMap } from "react-icons/fa"
-import { FiDollarSign, FiFacebook, FiGithub, FiInstagram, FiLinkedin } from "react-icons/fi"
+import { FiFacebook, FiGithub, FiInstagram, FiLinkedin } from "react-icons/fi"
 import discord from "@/assets/icons/discord.svg"
 import telegram from "@/assets/icons/telegram.svg"
 import { CardMultipleIcons } from "@/components/card-multiple-icons"
 import { AboutUs } from "@/components/home/about-us"
 import { Hero } from "@/components/home/hero"
 import { Materials } from "@/components/home/materials"
-import { CallToAction } from "@/components/ui/call-to-action"
 
 export default function Home() {
   return (
     <main className="w-full">
-      <CallToAction title="Mappa Microonde" caption="Dove scaldare il pranzo nei vari campus." icon={FaRegMap} />
-      <CallToAction title="DSU" icon={FiDollarSign} />
       <Hero />
       <Materials />
       <AboutUs />
