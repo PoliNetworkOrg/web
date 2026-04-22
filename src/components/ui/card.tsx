adb2527e579ef2e9d9fde7ea2fac7efde7662e09
Alessandro Giuseppe Gioia <alessandroogioiaa@gmail.com>
2026-04-22 21:36:22 +0200

    Card (#41)
    * Card initial commit
    
    * Text and icon gradients
    
    * Used figma classes
    
    * Moved card.tsx to components/ui/cards
    
    * refactor: move card.tsx into ui
    
    * Card initial commit
    
    * Text and icon gradients
    
    * Used figma classes
    
    * Moved card.tsx to components/ui/cards
    
    * refactor: move card.tsx into ui
    
    * chore: biome; fix: imports
    
    * aligned card.tsx between all cards
    
    * feat: optional gradient
    
    * fix: text transparency to see gradient
    
    * feat: added bg-background-blur to all cards
    
    * feat: add card-caption (#48)
    
    * created cardCaption.tsx
    
    * Updated card-caption to use react-icons
    
    * Adjusted font sizes
    
    * Updated card-caption to use react-icons
    
    * Adjusted font sizes
    
    * chore: biome
    
    * feat: typography
    
    * aligned card.tsx between all cards
    
    * fix: homepage cards layout
    
    * chore: biome
    
    * feat: add card-path-selection (#56)
    
    * created cardCaption.tsx
    
    * Updated card-caption to use react-icons
    
    * Adjusted font sizes
    
    * Added path selection cards
    
    * chore: biome
    
    * Updated card-caption to use react-icons
    
    * Adjusted font sizes
    
    * Added path selection cards
    
    * chore: biome
    
    * rm: card-caption from this branch
    
    * fix: typography
    
    * fix: imports and home layout
    
    * remove: bg-background-blur
    
    * feat: add card-course (#63)
    
    * feat: card-groups initial commit
    
    * fix: spacing between elements
    
    * chore: biome
    
    * fix: homepage layout for cards
    
    * remove: bg-background-blur
    
    * chore: biome
    
    * feat: add card-course-group (#67)
    
    * feat: card-course-group initial commit
    
    * chore: biome
    
    * fix: make text and icons black
    
    * feat: added cards to homepage; feat: added bg-background-blur to the card
    
    * remove: bg-background-blur
    
    * remove: truncate class
    
    * feat: secondary variant
    
    * chore: biome
    
    * chore: biome
    
    * fix: some of CodeRabbit's proposed fixes
    
    * required changes by @toto04 and CodeRabbit
    
    * feat: implemented @BIA3IA's card hover background
    
    * feat: added  class to Card
    
    * feat: CardTitle is now
    
    * fix: removed text sizes from the base card components
    
    * fix: CardTitle props type
    
    ---------
    
    Co-authored-by: Tommaso Morganti <tommaso.morganti01@gmail.com>
    Co-authored-by: Lorenzo Corallo <66379281+lorenzocorallo@users.noreply.github.com>

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,102 @@
+import * as React from "react"
+import type { IconType } from "react-icons"
+import { cn } from "@/lib/utils"
+import { CardHoverBackground } from "../card-icon/hover-background"
+import { Glass } from "../glass"
+import { Button } from "./button"
+
+function Card({
+  className,
+  size = "default",
+  hoverBackground = false,
+  children,
+  ...props
+}: React.ComponentProps<typeof Glass> & { size?: "default" | "sm"; hoverBackground?: boolean }) {
+  return (
+    <Glass
+      data-slot="card"
+      data-size={size}
+      className={cn(
+        "group/card relative flex h-66 w-78 flex-col gap-4 overflow-hidden rounded-[1.25rem] bg-background-blur text-card-foreground text-sm ring-foreground/10 has-[>img:first-child]:pt-0 data-[size=sm]:gap-3 data-[size=sm]:py-3 data-[size=sm]:has-data-[slot=card-footer]:pb-0 *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl",
+        className
+      )}
+      {...props}
+    >
+      {hoverBackground && <CardHoverBackground />}
+      {children}
+    </Glass>
+  )
+}
+
+function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-header"
+      className={cn(
+        "group/card-header @container/card-header grid auto-rows-min items-start gap-1 rounded-t-xl has-data-[slot=card-action]:grid-cols-[1fr_auto] has-data-[slot=card-description]:grid-rows-[auto_auto] group-data-[size=sm]/card:px-3 [.border-b]:pb-4 group-data-[size=sm]/card:[.border-b]:pb-3",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function CardTitle({ gradient = true, className, ...props }: React.ComponentProps<"h3"> & { gradient?: boolean }) {
+  return (
+    <h3
+      data-slot="card-title"
+      className={cn(
+        `${gradient ? "bg-linear-to-b from-blue-secondary to-blue-primary bg-clip-text text-transparent" : ""} font-medium leading-snug group-data-[size=sm]/card:text-base`,
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function CardDescription({ className, ...props }: React.ComponentProps<"div">) {
+  return <div data-slot="card-description" className={cn("text-muted-foreground", className)} {...props} />
+}
+
+function CardAction({
+  className,
+  icon: Icon,
+  iconSize = "normal",
+  gradient = true,
+  ...props
+}: React.ComponentProps<"div"> & { icon: IconType; iconSize?: "small" | "normal" | "large"; gradient?: boolean }) {
+  const gradientId = React.useId()
+
+  return (
+    <div
+      data-slot="card-action"
+      className={cn("col-start-2 row-span-2 row-start-1 self-auto justify-self-end", className)}
+      {...props}
+    >
+      {gradient && (
+        <svg width="0" height="0" className="absolute" aria-hidden="true" focusable="false">
+          <linearGradient id={gradientId} x1="0%" y1="100%" x2="0%" y2="0%">
+            <stop offset="0%" className="text-blue-secondary" stopColor="currentColor" />
+            <stop offset="100%" className="text-blue-primary" stopColor="currentColor" />
+          </linearGradient>
+        </svg>
+      )}
+
+      <Icon
+        size={iconSize === "small" ? "1.125rem" : iconSize === "normal" ? "2rem" : "3.5rem"}
+        fill={gradient ? `url(#${gradientId})` : "currentColor"}
+        stroke={gradient ? `url(#${gradientId})` : "currentColor"}
+      />
+    </div>
+  )
+}
+
+function CardContent({ className, ...props }: React.ComponentProps<"div">) {
+  return <div data-slot="card-content" className={cn("group-data-[size=sm]/card:px-3", className)} {...props} />
+}
+
+function CardBottomButton({ className, ...props }: React.ComponentProps<typeof Button>) {
+  return <Button data-slot="card-footer" className={cn("self-end", className)} {...props} />
+}
+
+export { Card, CardHeader, CardBottomButton, CardTitle, CardAction, CardDescription, CardContent }
