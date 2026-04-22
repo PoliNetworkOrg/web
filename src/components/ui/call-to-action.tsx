d8a963a18cc2aaca1d2d3ab1291a63c9127a927c
diubi <alessandroogioiaa@gmail.com>
2026-04-22 22:49:33 +0200

    feat: hover effect

--- src/components/ui/call-to-action.tsx
+++ src/components/ui/call-to-action.tsx
@@ -17,7 +17,7 @@ export function CallToAction({
     <Link href={href}>
       <Card className="h-fit w-80.75 px-8 py-5.25">
         <CardHeader className={`flex items-center ${caption ? "gap-2" : "gap-10"}`}>
-          <div className="rounded-[0.625rem] bg-blue-tertiary p-2.5">
+          <div className="rounded-[0.625rem] bg-blue-tertiary p-2.5 transition-all duration-500 group-hover/card:p-3">
             <CardAction icon={icon} gradient={false} iconSize="sm" />
           </div>
           <CardTitle className="typo-title-large">{title}</CardTitle>
