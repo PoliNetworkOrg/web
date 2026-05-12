1b0296ffe329c0fa1162e822a49773cb3742947b
Bianca <ianoselbiancaroberta02@gmail.com>
2026-05-12 13:09:49 +0200

    feat: add responsive carousel in mobile

--- src/components/projects/collection.tsx
+++ src/components/projects/collection.tsx
@@ -1,81 +1,75 @@
 import { FiArrowDown, FiSearch, FiUploadCloud } from "react-icons/fi"
-import { CardIcon } from "@/components/card-icon"
+import { CardCaption } from "../card-caption"
 import { Button } from "../ui/button"
+import { Carousel, CarouselContent, CarouselDots, CarouselItem } from "../ui/carousel"
 import { Input } from "../ui/input"
 
-// TODO da sostituire con dati veri
 const collectionCards = [
   {
     title: "Title 1",
-    description:
-      "description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.",
+    caption:
+      "caption: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.",
     icon: FiUploadCloud,
-    size: "sm",
     href: "#",
   },
   {
     title: "Title 2",
-    description:
-      "description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.",
+    caption:
+      "caption: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.",
     icon: FiUploadCloud,
-    size: "sm",
     href: "#",
   },
   {
     title: "Title 3",
-    description:
-      "description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.",
+    caption:
+      "caption: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.",
     icon: FiUploadCloud,
-    size: "sm",
     href: "#",
   },
   {
     title: "Title 4",
-    description:
-      "description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.",
+    caption:
+      "caption: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.",
     icon: FiUploadCloud,
-    size: "sm",
     href: "#",
   },
   {
     title: "Title 5",
-    description:
-      "description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.",
+    caption:
+      "caption: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.",
     icon: FiUploadCloud,
-    size: "sm",
     href: "#",
   },
   {
     title: "Title 6",
-    description:
-      "description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.",
+    caption:
+      "caption: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.",
     icon: FiUploadCloud,
-    size: "sm",
     href: "#",
   },
   {
     title: "Title 7",
-    description:
-      "description: Lorem ipsum dolor sit amet, consectet ur adipiscing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.",
+    caption:
+      "caption: Lorem ipsum dolor sit amet, consectet ur adipiscing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.",
     icon: FiUploadCloud,
-    size: "sm",
     href: "#",
   },
   {
     title: "Title 8",
-    description:
-      "description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.",
+    caption:
+      "caption: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua.",
     icon: FiUploadCloud,
-    size: "sm",
     href: "#",
   },
 ] as const
 
 export function Collection() {
   return (
-    <section className="mx-auto flex min-h-screen max-w-400 flex-col items-center justify-center gap-22 px-4">
+    <section className="mx-auto flex min-h-screen max-w-400 flex-col items-center justify-center gap-12 px-4 sm:gap-22">
       <div className="flex flex-col items-center gap-8">
-        <h2 className="typo-display-medium text-center">Esplora la raccolta completa dei progetti</h2>
+        <h2 className="typo-headline-medium sm:typo-display-medium text-center">
+          Esplora la raccolta completa dei progetti
+        </h2>
         <div className="flex w-full justify-center">
           <Input
             icon={<FiSearch className="h-5 w-5" />}
@@ -88,11 +82,10 @@ export function Collection() {
         </div>
       </div>
 
-      <div className="flex flex-col gap-12">
-        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
-          {/* TODO sostituire la card con la versione corretta */}
+      <div className="hidden flex-col gap-12 sm:flex">
+        <div className="grid grid-cols-2 justify-items-center gap-6 xl:grid-cols-4">
           {collectionCards.map((card) => (
-            <CardIcon key={card.title} {...card} className="h-full" />
+            <CardCaption key={card.title} {...card} />
           ))}
         </div>
         <div className="flex justify-center">
@@ -102,6 +95,21 @@ export function Collection() {
           </Button>
         </div>
       </div>
-    </section>
+
+      <div className="flex w-full items-center justify-center sm:hidden">
+        <Carousel className="w-full">
+          <CarouselContent>
+            {collectionCards.map((card) => (
+              <CarouselItem key={card.title}>
+                <div className="flex justify-center">
+                  <CardCaption {...card} />
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselDots className="mt-8" />
+        </Carousel>
+      </div>
+    </section >
   )
 }
