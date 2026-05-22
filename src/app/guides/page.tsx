609632feb5c57e925f34789c54e72b9a5ba21d26
Bianca <ianoselbiancaroberta02@gmail.com>
2026-05-22 11:43:42 +0200

    feat: replace GuideHero component with Hero component in GuidePage

--- src/app/guides/page.tsx
+++ src/app/guides/page.tsx
@@ -1,7 +1,7 @@
 import { FiUserPlus } from "react-icons/fi"
 import GuideContent from "@/components/guides/content"
-import GuideHero from "@/components/guides/hero"
 import { Button } from "@/components/ui/button"
+import { Hero } from "@/components/ui/hero"
 
 const guidesInfo = {
   title: "Le guide per Ingegneria Informatica",
@@ -36,20 +36,24 @@ export default function GuidePage() {
   return (
     <section className="min-h-screen w-full">
       <div className="mx-auto flex w-full max-w-350 flex-col gap-35 px-12 pt-58 pb-18">
-        <GuideHero />
+        <Hero title="Guide" description="Una raccolta di guide realizzate dai membri del Network" />
         <GuideContent {...guidesInfo} />
       </div>
 
-      <div className="flex w-full justify-end px-12 pb-18">
+      {/* Desktop */}
+      <div className="hidden w-full justify-end px-12 pb-18 sm:flex">
         <Button variant="tertiaryBlur" size="lg" className="text-blue-secondary">
           <FiUserPlus />
           Sei una matricola?
         </Button>
       </div>
 
-      <div className="mx-auto flex w-full max-w-350 flex-col px-12 pb-18">
+      <div className="mx-auto hidden w-full max-w-350 flex-col px-12 pb-18 sm:flex">
         <GuideContent {...guidesGeneral} />
       </div>
+
+      {/* Mobile */}
+
     </section>
   )
 }
