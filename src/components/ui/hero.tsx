c77ef1d8fdf746a62799180d09261fac9b3af2ad
Bianca <ianoselbiancaroberta02@gmail.com>
2026-05-22 10:20:19 +0200

    style: format Hero component for improved readability

--- src/components/ui/hero.tsx
+++ src/components/ui/hero.tsx
@@ -1,12 +1,10 @@
 export function Hero({ title, description }: { title: string; description: string }) {
-    return (
-        <div className="flex flex-col items-center gap-6">
-            <h2 className="typo-display-large sm:typo-display-extralarge w-fit bg-linear-to-b from-text-primary to-text-secondary bg-clip-text py-4 text-transparent sm:py-14">
-                {title}
-            </h2>
-            <p className="typo-title-large sm:typo-headline-small max-w-2xl text-center">
-                {description}
-            </p>
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center gap-6">
+      <h2 className="typo-display-large sm:typo-display-extralarge w-fit bg-linear-to-b from-text-primary to-text-secondary bg-clip-text py-4 text-transparent sm:py-14">
+        {title}
+      </h2>
+      <p className="typo-title-large sm:typo-headline-small max-w-2xl text-center">{description}</p>
+    </div>
+  )
 }
