6998c121c62643df501eb5baf94c1678cbe878b8
Tommaso Morganti <tommaso.morganti01@gmail.com>
2026-07-13 07:46:54 +0200

    fix: make call not fail on build

--- src/queries/guida-matricola.ts
+++ src/queries/guida-matricola.ts
@@ -3,7 +3,7 @@
 import { trpc } from "@/lib/backend"
 
 export async function getLatestGuidaMatricola() {
-  const guide = await trpc.web.guides_matricole.getLatestGuide.query()
+  const guide = await trpc.web.guides_matricole.getLatestGuide.query().catch(() => null)
   if (!guide) return null
 
   return { version: guide.version, date: guide.date, url: guide.file }
