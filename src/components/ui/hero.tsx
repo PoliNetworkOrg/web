9d5419ded882f755afd5ec2778c73fd13529b689
Ita_simo <118686320+Itasimo@users.noreply.github.com>
2026-08-28 22:41:26 +0200

    fix: correct gradient colors variable names in Hero component

--- src/components/ui/hero.tsx
+++ src/components/ui/hero.tsx
@@ -14,9 +14,9 @@ export function Hero({
       </h2>
       <p
         className={
-          // background: conic-gradient(from 151deg at 45.23% 50%, var(--accents-blue-secondary, #0069A8) 0%, var(--neutrals-text-primary, #020618) 50.48%, var(--accents-blue-secondary, #0069A8) 96.63%)
+          // background: conic-gradient(from 151deg at 45.23% 50%, var(--color-blue-secondary) 0%, var(--color-text-primary) 50.48%, var(--color-blue-secondary) 96.63%)
           gradientDescription
-            ? "typo-title-large sm:typo-display-small max-w-3xl bg-[conic-gradient(from_151deg_at_45.23%_50%,var(--accents-blue-secondary,#0069A8)_0%,var(--neutrals-text-primary,#020618)_50.48%,var(--accents-blue-secondary,#0069A8)_96.63%)] bg-clip-text text-center text-transparent"
+            ? "typo-title-large sm:typo-display-small max-w-3xl bg-[conic-gradient(from_151deg_at_45.23%_50%,var(--color-blue-secondary)_0%,var(--color-text-primary)_50.48%,var(--color-blue-secondary)_96.63%)] bg-clip-text text-center text-transparent"
             : "typo-title-large sm:typo-headline-small max-w-2xl text-center"
         }
       >
