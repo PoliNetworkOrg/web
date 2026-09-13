4fa1542df4595af0d084a474781184e74871f87b
Filippo <enderf5027@gmail.com>
2026-09-13 23:29:45 +0200

    fix: home page align hero with center

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -5,8 +5,8 @@ import { GroupSearch } from "./group-search"
 
 export function Hero() {
   return (
-    <section className="flex min-h-screen flex-col gap-40 px-6 pt-67 pb-15 sm:gap-70 sm:px-12">
-      <div className="flex flex-col items-center gap-17.5 text-center">
+    <section className="flex min-h-screen flex-col gap-10 px-6 pt-67 pb-15 sm:px-12">
+      <div className="flex flex-1 justify-center flex-col items-center gap-17.5 text-center">
         <h1 className="typo-display-small sm:typo-display-medium md:typo-display-large w-fit max-w-4xl bg-linear-to-b from-text-primary to-text-secondary bg-clip-text text-transparent">
           Trova gruppi, risorse e supporto tra gli studenti del Polimi
         </h1>
@@ -21,7 +21,7 @@ export function Hero() {
         </Button>
       </div>
 
-      <div className="flex items-end justify-end">
+      <div className="mt-auto flex justify-end">
         <Button variant="tertiaryBlur" size="lg" className="text-blue-secondary" asChild>
           <Link href="/matricole">
             <FiUserPlus />
